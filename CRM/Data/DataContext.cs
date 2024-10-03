376828e66a6f7ee8b566439b1b5413abb33c9ed4
Flavio-Converso <fla.converso@gmail.com>
2024-10-03 17:01:10 +0200

    Add initial EF Core migration and update DataContext
    Updated DataContext.cs to configure many-to-many relationships customized table names.
    
    Added migration file 20241003145821_InitCreate.cs to create tables.
    
    Generated 20241003145821_InitCreate.Designer.cs to define the model for the migration.
    
    Introduced DataContextModelSnapshot.cs, an auto-generated file for managing database migrations, including entity configurations, relationships, and PostgreSQL-specific settings.

--- CRM/Data/DataContext.cs
+++ CRM/Data/DataContext.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CRM.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRM.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Availability> Availabilities { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CompanyType> CompanyTypes { get; set; }
+        public DbSet<CompanyWorker> CompanyWorkers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OfferedService> OfferedServices { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure many-to-many relationships
+
+            // Appointment and OfferedService
+            modelBuilder.Entity<Appointment>()
+                .HasMany(a => a.OfferedServices)
+                .WithMany(os => os.Appointments)
+                .UsingEntity(j => j.ToTable("AppointmentServices"));
+
+            // Company and OfferedService
+            modelBuilder.Entity<Company>()
+                .HasMany(c => c.OfferedServices)
+                .WithMany(os => os.Companies)
+                .UsingEntity(j => j.ToTable("CompanyOfferedServices"));
+
+            // Customer and Appointment
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Appointments)
+                .WithMany(a => a.Customers)
+                .UsingEntity(j => j.ToTable("CustomerAppointments"));
         }
     }
 }
---
+++ CRM/Migrations/20241003145821_InitCreate.cs
@@ -0,0 +1,319 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyTypes",
+                columns: table => new
+                {
+                    CompanyTypeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyTypes", x => x.CompanyTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfferedServices",
+                columns: table => new
+                {
+                    OfferedServiceId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfferedServices", x => x.OfferedServiceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompanyTypeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Companies_CompanyTypes_CompanyTypeId",
+                        column: x => x.CompanyTypeId,
+                        principalTable: "CompanyTypes",
+                        principalColumn: "CompanyTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyOfferedServices",
+                columns: table => new
+                {
+                    CompaniesCompanyId = table.Column<int>(type: "integer", nullable: false),
+                    OfferedServicesOfferedServiceId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyOfferedServices", x => new { x.CompaniesCompanyId, x.OfferedServicesOfferedServiceId });
+                    table.ForeignKey(
+                        name: "FK_CompanyOfferedServices_Companies_CompaniesCompanyId",
+                        column: x => x.CompaniesCompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyOfferedServices_OfferedServices_OfferedServicesOffer~",
+                        column: x => x.OfferedServicesOfferedServiceId,
+                        principalTable: "OfferedServices",
+                        principalColumn: "OfferedServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyWorkers",
+                columns: table => new
+                {
+                    WorkerId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyWorkers", x => x.WorkerId);
+                    table.ForeignKey(
+                        name: "FK_CompanyWorkers_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompanyWorkerId = table.Column<int>(type: "integer", nullable: false),
+                    CompanyId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentId);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId");
+                    table.ForeignKey(
+                        name: "FK_Appointments_CompanyWorkers_CompanyWorkerId",
+                        column: x => x.CompanyWorkerId,
+                        principalTable: "CompanyWorkers",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Availabilities",
+                columns: table => new
+                {
+                    AvailabilityId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Day = table.Column<int>(type: "integer", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    CompanyWorkerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Availabilities", x => x.AvailabilityId);
+                    table.ForeignKey(
+                        name: "FK_Availabilities_CompanyWorkers_CompanyWorkerId",
+                        column: x => x.CompanyWorkerId,
+                        principalTable: "CompanyWorkers",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentServices",
+                columns: table => new
+                {
+                    AppointmentsAppointmentId = table.Column<int>(type: "integer", nullable: false),
+                    OfferedServicesOfferedServiceId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentServices", x => new { x.AppointmentsAppointmentId, x.OfferedServicesOfferedServiceId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentServices_Appointments_AppointmentsAppointmentId",
+                        column: x => x.AppointmentsAppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "AppointmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentServices_OfferedServices_OfferedServicesOfferedS~",
+                        column: x => x.OfferedServicesOfferedServiceId,
+                        principalTable: "OfferedServices",
+                        principalColumn: "OfferedServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAppointments",
+                columns: table => new
+                {
+                    AppointmentsAppointmentId = table.Column<int>(type: "integer", nullable: false),
+                    CustomersCustomerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAppointments", x => new { x.AppointmentsAppointmentId, x.CustomersCustomerId });
+                    table.ForeignKey(
+                        name: "FK_CustomerAppointments_Appointments_AppointmentsAppointmentId",
+                        column: x => x.AppointmentsAppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "AppointmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerAppointments_Customers_CustomersCustomerId",
+                        column: x => x.CustomersCustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CompanyId",
+                table: "Appointments",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CompanyWorkerId",
+                table: "Appointments",
+                column: "CompanyWorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentServices_OfferedServicesOfferedServiceId",
+                table: "AppointmentServices",
+                column: "OfferedServicesOfferedServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Availabilities_CompanyWorkerId",
+                table: "Availabilities",
+                column: "CompanyWorkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CompanyTypeId",
+                table: "Companies",
+                column: "CompanyTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyOfferedServices_OfferedServicesOfferedServiceId",
+                table: "CompanyOfferedServices",
+                column: "OfferedServicesOfferedServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyWorkers_CompanyId",
+                table: "CompanyWorkers",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAppointments_CustomersCustomerId",
+                table: "CustomerAppointments",
+                column: "CustomersCustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentServices");
+
+            migrationBuilder.DropTable(
+                name: "Availabilities");
+
+            migrationBuilder.DropTable(
+                name: "CompanyOfferedServices");
+
+            migrationBuilder.DropTable(
+                name: "CustomerAppointments");
+
+            migrationBuilder.DropTable(
+                name: "OfferedServices");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "CompanyWorkers");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "CompanyTypes");
+        }
+    }
+}
